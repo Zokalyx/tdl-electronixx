39ccdcc038036a5a1f0a39c3d6723ab700a3499e
Maximiliano Torre <mtorre@southernhorizon.tech>
2025-06-10 11:26:17 -0300

    Agregar funcionalidad de log out

--- lib/tdl_web/router.ex
+++ lib/tdl_web/router.ex
@@ -21,10 +21,11 @@ defmodule TdlWeb.Router do
     get "/", PageController, :home
     live "/editor/:id", Editor
 
-    get "/login", SessionController, :new
-    post "/login", SessionController, :create
     get "/register", UserRegistrationController, :new
     post "/register", UserRegistrationController, :create
+    get "/login", SessionController, :new
+    post "/login", SessionController, :create
+    delete "/logout", SessionController, :delete
 
     get "/files", FilesController, :index
     get "/files/:id", FilesController, :folder
